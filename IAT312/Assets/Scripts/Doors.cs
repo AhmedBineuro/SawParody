0d2b928670127b1cb23ae32fd636824d6fc11a58
Dynamic Bineuro <dynamic.bineuro@gmail.com>
2023-10-28 11:42:51 -0700

    Initial commit

---
+++ IAT312/Assets/Scripts/Doors.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public Animator door;
+    public GameObject openText;
+
+    public bool inReach;
+
+    void Start()
+    {
+        inReach = false;
+        door.SetBool("open", false);
+        door.SetBool("closed", true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (inReach && Input.GetButtonDown("Interact"))
+        {
+            if(door.GetBool("closed"))
+                DoorOpens();
+            else
+                DoorCloses();
+        }
+    }
+    void DoorOpens ()
+    {
+        door.SetBool("open", true);
+        door.SetBool("closed", false);
+
+    }
+
+    void DoorCloses()
+    {
+        door.SetBool("open", false);
+        door.SetBool("closed", true);
+    }
+
+}
