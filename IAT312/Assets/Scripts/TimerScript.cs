38e85de121286fbb1fd55872c8edd47d046e5b88
Dynamic Bineuro <dynamic.bineuro@gmail.com>
2023-10-29 21:22:58 -0700

    Added timer and timer controller

---
+++ IAT312/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+    public float timeLeft;
+    public bool timerOn = false;
+    public TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerOn)
+        {
+            if(!text.enabled)
+                text.enabled = true;
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                updateTimer(timeLeft);
+            }
+            else
+            {
+                Debug.Log("Time is up");
+                timeLeft = 0;
+                timerOn = false;
+            }
+        }
+        else if (text.enabled)
+        {
+            text.enabled = false;
+        }
+    }
+    void updateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes= Mathf.FloorToInt(currentTime/60);
+        float seconds= Mathf.FloorToInt(currentTime%60);
+        text.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+    }
+}
